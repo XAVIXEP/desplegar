686e15693702b61e2695c544598ec4e368ce2f13
XAVIXEP <xavixep4@gmail.com>
2024-12-19 09:42:16 +0100

    dist es bo

---
+++ js/about.a9f92a77.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdesplegar"]=self["webpackChunkdesplegar"]||[]).push([[594],{1417:function(e,n,s){s.r(n),s.d(n,{default:function(){return i}});var t=function(){var e=this;e._self._c;return e._m(0)},u=[function(){var e=this,n=e._self._c;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],a=s(1656),l={},r=(0,a.A)(l,t,u,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.a9f92a77.js.map
